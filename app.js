abe60a653d685558172276ff8ed3bf9c21233904
JonWise82 <133918862+JonWise82@users.noreply.github.com>
2023-09-05 22:50:45 +0100

    keep score

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 function playGame () {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let numberRounds = prompt("Please enter number of rounds");
+    
     let i = 0;
     do {
-        let playerChoice = prompt("Please enter your choice of rock, paper or scissors");
+        let playerChoice = prompt("Please enter your choice of rock, paper or" +
+            " scissors");        
+        playerChoice = playerChoice.toLowerCase();
         let computerChoice = randomChoice();
-        decideResult(playerChoice, computerChoice);
+        let result = decideResult(playerChoice, computerChoice);
+        
+        //manage score
+        if (result === "resultComputer") {
+            computerScore += 1;
+        }
+        if (result === "resultPlayer") {
+            playerScore += 1;
+        }
+        console.log("Player " + playerScore + " - " + computerScore + 
+        " Computer");
+        
         i++;
-    } while (i < 5);
+    } while (i < numberRounds);
+
+    console.log("FINAL SCORE: Player " + playerScore + " - " + computerScore + 
+        " Computer");
 }
 
 function randomChoice () {
@@ -16,33 +37,41 @@ function randomChoice () {
 
 function decideResult (playerChoice, computerChoice) {
     switch (playerChoice) {
-        case "Rock":
+        case "rock":
             if (computerChoice === "Rock") {
                 console.log("Computer chose rock, so it's a draw");
+                return "resultDraw";
             } else if (computerChoice === "Paper") {
                 console.log("Computer chose paper, so you lose");
+                return "resultComputer";
             } else if (computerChoice === "Scissors") {
                 console.log("Computer chose scissors, so you win!");
+                return "resultPlayer";
             }
             break;
-        case "Scissors":
+        case "scissors":
             if (computerChoice === "Rock") {
                 console.log("Computer chose rock, so you lose");
+                return "resultComputer";
             } else if (computerChoice === "Paper") {
                 console.log("Computer chose paper, so you win!");
+                return "resultPlayer";
             } else if (computerChoice === "Scissors") {
                 console.log("Computer chose scissors, so it's a draw");
+                return "resultDraw";
             }
             break;
-        case "Paper":
+        case "paper":
             if (computerChoice === "Rock") {
                 console.log("Computer chose rock, so you win!");
+                return "resultPlayer";
             } else if (computerChoice === "Paper") {
                 console.log("Computer chose paper, so it's a draw");
+                return "resultDraw";
             } else if (computerChoice === "Scissors") {
                 console.log("Computer chose scissors, so you lose");
+                return "resultComputer";
             }
             break;
     }
 }
-
